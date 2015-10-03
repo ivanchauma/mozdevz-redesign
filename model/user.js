26b8978a83612fa72bb28a015025c9c0e0cc05a3
Adilson Mandlate <adilsonmandlate@gmail.com>
2015-10-03 07:30:47 -0300

    User model

--- model/user.js
+++ model/user.js
@@ -1,7 +1,65 @@
 var mongoose = require('mongoose');
 
 var schema = new mongoose.Schema({
-	// Define User Schema here.
+	name: {
+		type: String,
+		unique: true
+	},
+	password: {
+		type: String,
+		require: true
+	},
+	level: { // Nivel de usuario (Os niveis sao 1 e 2, sendo 1 - Administrador e 2 - Common user)
+		type: Number
+	},
+	regDate: { // Data de registo
+		type: Date,
+		default: Date.now
+	},
+	local: { // Local de residencia, etc...
+		res: {
+			type: String
+		}
+	},
+	cont: { // Informacoes de contacto
+		cel: { // Celular
+			type: String
+		},
+		email: {
+			type: String
+		}
+	},
+	det: { // Detalhes do usuario
+		gen:{ // Genero
+			type: String
+		},
+		desc:{ // Descricao
+			type: String
+		},
+		habil: [{
+			name: {
+				type: String
+			}
+		}],
+
+		port: [{ // Array de portfolio (com nome, link)
+			name: {
+				type: String
+			},
+			link: {
+				type: String
+			},
+			ano: {
+				type: String // Ou date
+			}
+		}]
+	},
+
+	//  Relacionado ao forum...
+	post: [{
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'Post'
+	}]
 });
 
 module.exports = exports = mongoose.model('User', schema);
