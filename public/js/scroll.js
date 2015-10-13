62248c85595670f40a80a5d0eff154297d832398
Adilson Mandlate <adilsonmandlate@gmail.com>
2015-10-13 03:16:24 -0300

    Layout defs

--- public/js/scroll.js
+++ public/js/scroll.js
@@ -1,5 +1,25 @@
 window.onload = function(){
 	$(document).on('scroll', onScroll);
+
+	$('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+        $(document).off("scroll");
+        
+        $('a').each(function () {
+            $(this).removeClass('active');
+        })
+        $(this).addClass('active');
+      
+        var target = this.hash,
+            menu = target;
+        $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top+2
+        }, 500, 'swing', function () {
+            window.location.hash = target;
+            $(document).on("scroll", onScroll);
+        });
+    });
 }
 
 
