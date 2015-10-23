4bab02987619e30311500404a0d93e2d9d53ec3f
Adilson Mandlate <adilsonmandlate@gmail.com>
2015-10-23 02:34:15 -0300

    Heroku push

--- config/db.js
+++ config/db.js
@@ -1,3 +1,4 @@
 module.exports = {
-	'url': 'mongodb://127.0.0.1:27017/mozdevz'
+	// 'url': 'mongodb://127.0.0.1:27017/mozdevz'
+	'url': 'mongodb://admin:admin2015@ds041934.mongolab.com:41934/mozdevz'
 }
